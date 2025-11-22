3f25be93fbb4b3c2783f30cb7f54b9a9488a784a
João Victor <joaovictorsaraivaaraujo@gmail.com>
2025-11-22 12:31:28 -0300

    Validando e covertendo tipos de dados

--- src/extract.py
+++ src/extract.py
@@ -5,6 +5,10 @@ import pandas as pd
 df = pd.read_csv('sales_data_sample.csv', encoding='latin-1') # encoding definido prara evitar erros
 print(df.head())
 
+# mostra colunas com seus dtypes
+print(df.dtypes)
+
+
 # Lidando com contagem de valores ausentes
 print("Valores ausentes por coluna:")
 print(df.isnull().sum())
@@ -12,9 +16,29 @@ print(df.isnull().sum())
 # Validar e converter tipos de dados
 df['ORDERDATE'] = pd.to_datetime(df['ORDERDATE'])
 
-df['MONTHID'] = df['MONTHID'].astype(int)
+df['MONTH_ID'] = df['MONTH_ID'].astype(int)
 
-df['YEARID'] = df['YEARID'].astype(int)
+df['YEAR_ID'] = df['YEAR_ID'].astype(int)
+
+df['PRODUCTLINE'] = df['PRODUCTLINE'].astype('category')
+
+df['STATUS'] = df['STATUS'].astype('category')
+
+df['CUSTOMERNAME'] = df['CUSTOMERNAME'].astype(str)
+
+df['CITY'] = df['CITY'].astype(str)
+
+df['STATE'] = df['STATE'].astype(str)
+
+df['COUNTRY'] = df['COUNTRY'].astype(str)
+
+df['TERRITORY'] = df['TERRITORY'].astype(str)
+
+df['CONTACTLASTNAME'] = df['CONTACTLASTNAME'].astype(str)
+
+df['CONTACTFIRSTNAME'] = df['CONTACTFIRSTNAME'].astype(str)
+
+df['DEALSIZE'] = df['DEALSIZE'].astype(str)
 
 print("\nNOME E TIPO: =========================")
 print(df.dtypes)
