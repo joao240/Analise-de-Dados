04381de7fe25bd6c3cb5d3d1155ebf61075bb178
João Victor <joaovictorsaraivaaraujo@gmail.com>
2026-01-04 18:23:15 -0300

    Finalizado o analysis e graficos

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,7 @@
 from src.extract import extract_data
 from src.transform import calculation_total_value
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def main():
     df = extract_data()
@@ -16,25 +17,57 @@ def total_value(df):
     print(f"Faturamento total: {faturamento_total}")
 
 def produtos_mais_vendidos(df):
-    print("\n🏆 TOP 10 PRODUTOS MAIS VENDIDOS:")
-    ranking = df.groupby('PRODUCTCODE')['QUANTITYORDERED'].sum().sort_values(ascending=False)
-    print(ranking.head(10))
-
+    ranking = df.groupby('PRODUCTCODE')['QUANTITYORDERED'].sum().sort_values(ascending=False).head(10)
+    # 1 - Criar grafico
+    plt.figure(figsize=(10,6)) # define o tamanho que vai ser o grafico 
+    ranking.plot(kind='bar', color='skyblue') # Plota os dados como barras
+    # 2 - Personalizar grafico
+    plt.title('Top 10 Produtos Mais Vendidos') # Titulo do grafico
+    plt.xlabel('Código do Produto') # Nome do eixo X
+    plt.ylabel('Quantidade Vendida') # Nome do eixo Y
+    plt.xticks(rotation=90) # Inclina os nomes para não amontoar
+    # 3 - Exibir grafico
+    plt.show() 
+     
 def paises_com_mais_vendas(df):
-    print("\n🌍 TOP 10 PAÍSES COM MAIS VENDAS:")
-    ranking = df.groupby('COUNTRY')['QUANTITYORDERED'].sum().sort_values(ascending=False)
-    print(ranking.head(10))
+    ranking = df.groupby('COUNTRY')['QUANTITYORDERED'].sum().sort_values(ascending=False).head(10)
+
+    plt.figure(figsize=(10, 8)) 
+    
+    # 1. Criar o gráfico de pizza
+    ranking.plot(
+        kind='pie', 
+        autopct='%1.1f%%',      # Mostra a porcentagem dentro da fatia
+        startangle=140,         # Gira o gráfico para começar em um ângulo melhor
+        colors=plt.cm.Paired.colors # Usa uma paleta de cores variadas e bonitas
+    )
+
+    # 2. Personalizar
+    plt.title('Participação dos Top 10 Países nas Vendas')
+    plt.ylabel('') # Remove o nome da coluna que fica na vertical e atrapalha o grafico
+
+    plt.show()
 
 def evolucao_vendas_mensal(df):
     df['ORDERDATE'] = pd.to_datetime(df['ORDERDATE'])
     df['YEAR_MONTH'] = df['ORDERDATE'].dt.to_period('M')
     vendas_mensais = df.groupby('YEAR_MONTH')['TOTAL_VALUE'].sum()
-    print("\n📈 EVOLUÇÃO DAS VENDAS MENSAIS:")
-    print(vendas_mensais.head(10))
     
+    plt.figure(figsize=(12,5))
+
+    vendas_mensais.index = vendas_mensais.index.astype(str)
+
+    plt.plot(vendas_mensais.index, vendas_mensais.values, marker='o', linestyle='-')
+
+    plt.title('Evolução das Vendas Mensais')
+    plt.grid(True, linestyle='--', alpha=0.6)
+    plt.xticks(rotation=45)
+    plt.show()
+
 def ticket_medio(df):
     faturamento_total = df['TOTAL_VALUE'].sum()
     numero_pedidos = df['ORDERNUMBER'].nunique()
     ticket_medio = faturamento_total / numero_pedidos
     print(f"\n🎟️ TICKET MÉDIO: {ticket_medio}")
     
+
